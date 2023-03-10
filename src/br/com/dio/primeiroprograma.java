82ea61cee75bc6e67fefd33f6429213478d58858
tmonestudio <tmonestudio@gmail.com>
2023-03-10 17:02:06 -0300

    First commit

---
+++ src/br/com/dio/primeiroprograma.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class primeiroprograma {
+
+	public static void main(String[] args) {
+			
+		Gato gato = new Gato(null, null, null);
+		
+		System.out.println(gato);
+	}
+
+}
